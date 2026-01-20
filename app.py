f81300bfae2643d189706afa84de56b3ec4ffe2a
ivillenadelfingdl-droid <ivillena.delfingdl@gmail.com>
2026-01-19 20:20:55 -0600

    Versión final

---
+++ app.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+# Leer el dataset
+car_data = pd.read_csv("vehicles_us.csv")
+
+# Encabezado de la aplicación
+st.title("Análisis Exploratorio de Datos de Vehículos")
+
+# Crear un botón para la generación del histograma
+if st.button('Histograma de Año del Modelo'):
+    # Crear un histograma de la columna 'year_model' o, si no existe, de la primera columna
+    if 'model_year' in car_data.columns:
+        fig_hist = px.histogram(car_data, x='model_year',
+                                title='Histograma de Año del Modelo')
+    else:
+        fig_hist = px.histogram(
+            car_data, x=car_data.columns[0], title=f'Histograma de {car_data.columns[0]}')
+    fig_hist.show()
+
+
+# Crear un botón para el gráfico de dispersión
+if st.button('Gráfico de Dispersión "Año vs Odómetro"'):
+    # Crear un gráfico de dispersión (scatter plot) entre 'odometer' y 'model_year' si existen, y si no, expresarlo
+    if 'odometer' in car_data.columns and 'model_year' in car_data.columns:
+        fig_scatter = px.scatter(car_data, x='model_year',
+                                 y='odometer', title='Año vs Odómetro')
+        fig_scatter.show()
+    else:
+        st.write(
+            "No se encontraron las columnas 'odometer' y 'price' para crear el scatter plot.")
